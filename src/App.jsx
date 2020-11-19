2239ac07fff4aed640cf8678e3c8fc39623c5e2d
barthflo <flrnt.barth@gmail.com>
2020-11-19 20:31:33 +0100

    potion cards design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Potions from './components/Potions/Potions';
+import {useState} from 'react';
+
 
 function App() {
+  const [activePotions, setActivePotions] = useState("Love");
+    const toggleActive = (potions) =>{
+        activePotions!== potions && setActivePotions(potions);
+    };
   return (
     <div className="App">
       <h1>Marmipotion</h1>
+      <ul className="list-unstyled">
+          <li className="text-light"><button onClick={() => toggleActive("Love") }className="btn btn-outline-dark">Love Potions</button></li>
+          <li className="text-light"><button onClick={()=> toggleActive("Strength") }className="btn btn-outline-dark">Strength Potions</button></li>
+      </ul>
+      <Potions activePotions = {activePotions} />
     </div>
   );
 }
---
+++ src/components/Potions/PotionCard/PotionCard.jsx
@@ -0,0 +1,24 @@
+import './PotionCard.css';
+
+function PotionCard() {
+    return (
+        <div className = "potion-card card mb-4">
+            <div className="card-header d-flex flex-column align-items-center ">
+                <h3>Love me tender</h3>
+                <figure className="mt-4">
+                    <img className="w-100 h-100" src="https://i.pinimg.com/564x/20/07/65/2007659e3bc27affa1df4d36823aa079.jpg"/>
+                </figure>
+            </div>
+            <div className="card-body">
+                <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde ex architecto tempora assumenda? Aperiam harum odit, consectetur quod adipisci consequatur? Neque ipsam, debitis illo praesentium architecto voluptate assumenda temporibus.</p>
+
+            </div>
+            <div className="card-footer d-flex justify-content-center">
+                <button className="btn btn-outline-light">See Recipe</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default PotionCard
