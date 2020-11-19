d75967145f21e3153a2e0d33606459efb7b7bc35
theo.atlani@gmail.com <theo.atlani@gmail.com>
2020-11-19 19:52:39 +0100

    modification App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Marmipotion</h1>
+      <Home />
     </div>
   );
 }
