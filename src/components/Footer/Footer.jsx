d6327ffa14206ef50e645ff39fa7d42acef4580d
lolarne <rne.lola@gmail.com>
2020-11-20 01:23:20 +0100

    add footer but not responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,33 @@
 import './Footer.css';
+import facebookL from '../../assets/socialLogo/facebookLight.png';
+import facebookD from '../../assets/socialLogo/facebookDark.png';
+import instagramL from '../../assets/socialLogo/instagramLight.png';
+import instagramD from '../../assets/socialLogo/instagramDark.png';
+import twitterL from '../../assets/socialLogo/twitterLight.png';
+import twitterD from '../../assets/socialLogo/twitterDark.png';
+import youtubeL from '../../assets/socialLogo/youtubeLight.png';
+import youtubeD from '../../assets/socialLogo/youtubeDark.png';
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <div className="footer">
-            <div>
-                <p>social network</p>
+            <div className="lineUp">
+                <div className="social">
+                    <img src={facebookL} alt="social network" />
+                    <img src={instagramL} alt="social network" />
+                    <img src={twitterL} alt="social network" />
+                    <img src={youtubeL} alt="social network" />
+                </div>
+                <div className="contact">
+                    <p>CONTACT</p>
+                    <p>Green carrier pigeon, call name: <span>'Sylvain-Louis'</span></p>
+                    <p>Address: <span>Seventh smoking tree in the Candy Forest</span></p>
+                </div>
             </div>
-            <div>
-                <p>pigeon vert</p>
-            </div>
-            <div>
-                <p>copyrigth - date</p>
+
+            <div className="copyright">
+                <p>Made with ♥ by Marmipotion</p>
+                <p>©Marmipotion - 1000-1254</p>
             </div>
         </div>
     )
