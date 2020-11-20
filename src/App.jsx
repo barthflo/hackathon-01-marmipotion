4055cd5ca00f79aa9d62f1c313a5111d3262e47f
barthflo <flrnt.barth@gmail.com>
2020-11-20 02:53:48 +0100

    fetch api working

--- src/App.jsx
+++ src/App.jsx
@@ -175,23 +175,25 @@ export const potions = [
     ],
   },
 ]
-const apiKey = "gmDjyPYUqAcIKx_nsREfhj5NQUFqdMlFIzUuCnuXrV8";
-const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
 function App() {
   const [plants, setPlants] = useState([]);
   useEffect(() => {
-    fetch(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants?token=${apiKey}`)
+    fetch(`https://plantsapi.herokuapp.com/plants`)
     .then(res => res.json())
-    .then(res => setPlants(res))
+    .then(res => {
+      setPlants(res.data);
+      });
   }, [])
-  console.log(plants);
+  
   const [activePotions, setActivePotions] = useState("Love");
     const toggleActive = (potions) =>{
         activePotions!== potions && setActivePotions(potions);
     };
+  console.log(plants);
   return (
     <div className="App">
+      {(plants.length === 0) ?"loading" : plants.map(plant => <li>{plant.common_name}</li>)}
       <Home />
       <Header activePotions = {activePotions} toggleActive={toggleActive}/>
       <Intro />
--- src/components/Potions/PotionCard/PotionCard.jsx
+++ src/components/Potions/PotionCard/PotionCard.jsx
@@ -1,6 +1,10 @@
 import './PotionCard.css';
+import {useState} from 'react';
+import {Collapse} from 'reactstrap';
 
 function PotionCard(props) {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle =() => setIsOpen(!isOpen);
     return (
         <div className = "potion-card card mb-4 p-4">
             <div className="card-header d-flex flex-column align-items-center ">
@@ -13,8 +17,13 @@ function PotionCard(props) {
                 <p className="text-center">{props.description}</p>
 
             </div>
-            <div className="card-footer d-flex justify-content-center">
-                <button className="btn btn-outline-light">See Recipe</button>
+            <div className="card-footer d-flex justify-content-center flex-column">
+                <button onClick={toggle}className="btn btn-outline-light">See Recipe</button>
+                <Collapse isOpen={isOpen}>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus illum mollitia nemo quam incidunt amet beatae. Reprehenderit et ad fugiat, quos aspernatur, nostrum dolorum tenetur deleniti dolorem voluptatibus, eius iure.</p>
+                </div>
+                </Collapse>
             </div>
             
         </div>
