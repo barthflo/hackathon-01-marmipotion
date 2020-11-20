03b8e4b7607db3aa8127c420ec162f8611a7a617
barthflo <flrnt.barth@gmail.com>
2020-11-20 11:03:38 +0100

    Added animations on scroll

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Intro.css";
 import Portrait from "../../assets/Portrait.png";
+import {Fade} from 'react-reveal';
 
 export default function Intro() {
   return (
@@ -12,21 +13,25 @@ export default function Intro() {
           the population !!
         </h2>
         <article>
+          <Fade left duration={1000}>
           <img src={Portrait} alt="Photography of Agnes" />
-          <div className='textContainer'>
-            <p>Hello noble visitor,</p>
-            <p>
-              I am Agnes Sampsom, witches from mothers to daughters for four
-              generations. The witch situation being difficult in these times,
-              so we decided to create the associations of mistreated witches, in
-              collaboration with other witches. The goal of this association is
-              to help witches to integrate into society. We share with you here
-              our best potions which will delight young and old, and will help
-              you in your daily life! I would particularly like to thank Théo,
-              Florent, Lola and Florian for helping us to carry out our project.
-            </p>
-            <p>Agnes Sampsom – Founder of the associations of mistreated witches</p>
-          </div>
+          </Fade>
+          <Fade  right duration={1000}>
+            <div className='textContainer'>
+              <p>Hello noble visitor,</p>
+              <p>
+                I am Agnes Sampsom, witches from mothers to daughters for four
+                generations. The witch situation being difficult in these times,
+                so we decided to create the associations of mistreated witches, in
+                collaboration with other witches. The goal of this association is
+                to help witches to integrate into society. We share with you here
+                our best potions which will delight young and old, and will help
+                you in your daily life! I would particularly like to thank Théo,
+                Florent, Lola and Florian for helping us to carry out our project.
+              </p>
+              <p>Agnes Sampsom – Founder of the associations of mistreated witches</p>
+            </div>
+          </Fade>
         </article>
       </div>
     </div>
